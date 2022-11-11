84f031b30bec6311e7f0a6a43e803d15be9b2c49
Andrey <777senchenko777@gmail.com>
2022-11-11 15:08:53 +0200

    Created a model

---
+++ Concentratio​n/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  Concentratio​n
+//
+//  Created by Andrey on 11.11.2022.
+//  Copyright © 2022 Andrii. All rights reserved.
+//
+
+import Foundation
+
+struct Card {
+    var isFaceUp = false
+    var isMatched = false
+    var identifier: Int 
+}
---
+++ Concentratio​n/Concentration.swift
@@ -0,0 +1,16 @@
+//
+//  Concentration.swift
+//  Concentratio​n
+//
+//  Created by Andrey on 11.11.2022.
+//  Copyright © 2022 Andrii. All rights reserved.
+//
+
+import Foundation
+
+class Concentration {
+    var cards = [Card]()
+    func chooseCard(at index: Int) {
+        
+    }
+}
